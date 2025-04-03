4ed38192d948fd8b8527ceef827b1614283eceea
anajuliarauber <anajuliarauber@hotmail.com>
2025-04-02 21:12:54 -0300

    feat: implement image upload functionality

---
+++ src/app/functions/upload-image.ts
@@ -0,0 +1,37 @@
+import { Readable } from 'node:stream'
+import { db } from '@/infra/db'
+import { schema } from '@/infra/db/schemas'
+import type { Either } from '@/shared/either'
+import { makeLeft, makeRight } from '@/shared/either'
+import { z } from 'zod'
+import { InvalidFileFormat } from './errors/invalid-file-format'
+
+const uploadImageInput = z.object({
+  fileName: z.string(),
+  contentType: z.string(),
+  contentStream: z.instanceof(Readable),
+})
+
+type UploadImageInput = z.input<typeof uploadImageInput>
+
+const allowedMimeTypes = ['image/jpeg', 'image/jpg', 'image/png', 'image/webp']
+
+export async function uploadImage(
+  input: UploadImageInput
+): Promise<Either<InvalidFileFormat, { url: string }>> {
+  const { contentStream, contentType, fileName } = uploadImageInput.parse(input)
+
+  if (!allowedMimeTypes.includes(contentType)) {
+    return makeLeft(new InvalidFileFormat())
+  }
+
+  //TODO: carregar a imagem para claudfare r2
+
+  await db.insert(schema.uploads).values({
+    name: fileName,
+    remoteKey: fileName,
+    remoteUrl: fileName,
+  })
+
+  return makeRight({ url: '' })
+}
