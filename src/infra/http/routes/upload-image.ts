bef833b0c3abc85878ee3cfdbf9fb80ab26bd32b
anajuliarauber <anajuliarauber@hotmail.com>
2025-04-04 21:53:21 -0300

    feat: validate file size

--- src/infra/http/routes/upload-image.ts
+++ src/infra/http/routes/upload-image.ts
@@ -21,23 +21,29 @@ export const uploadImageRoute: FastifyPluginAsyncZod = async server => {
       },
     },
     async (request, reply) => {
-      const uploadFile = await request.file({
+      const uploadedFile = await request.file({
         limits: {
           fileSize: 1024 * 1024 * 2, // 2MB
         },
       })
 
-      if (!uploadFile) {
+      if (!uploadedFile) {
         return reply.status(400).send({
           message: 'File is required',
         })
       }
 
       const result = await uploadImage({
-        fileName: uploadFile.filename,
-        contentType: uploadFile.mimetype,
-        contentStream: uploadFile.file,
+        fileName: uploadedFile.filename,
+        contentType: uploadedFile.mimetype,
+        contentStream: uploadedFile.file,
       })
+
+      if (uploadedFile.file.truncated) {
+        return reply.status(400).send({
+          message: 'File size limit reached',
+        })
+      }
 
       if (isRight(result)) {
         console.log(unwrapEither(result))
